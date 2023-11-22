0703d800fd350c7a07abb628101f8ec09979968a
CalinCretu <calincretu1412@gmail.com>
2023-11-22 16:36:18 +0100

    ADD - Hotel_names

--- src/index.php
+++ src/index.php
@@ -9,8 +9,66 @@
 </head>
 
 <body>
-  <div class="container-sm text-center">
-    <h1>prova</h1>
+<?php
+
+$hotels = [
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
+];
+?>
+
+  <div class="container-sm text-center my-5 py-5">
+    <table class="table">
+      <thead>
+        Hotels
+      </thead>
+      <tbody>
+        <?php 
+        foreach ($hotels as $key => $value){
+          var_dump($key, $value['name']);
+          $name = $value['name'];
+          ?>
+          <tr>
+          <th scope="col"><?php echo $name ?></th>
+          </tr>
+          <?php
+        };
+        ?>
+      </tbody>
+    </table>
   </div>
 
 </body>
